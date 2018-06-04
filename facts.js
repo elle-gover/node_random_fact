1a170e31ede5da50c0e18c968feeb8498421828c
elle <ellegover@gmail.com>
2018-06-04 16:47:27 -0400

    added more facts

--- facts.js
+++ facts.js
@@ -3,16 +3,18 @@
 
 function factRefresh(){
     const facts = [
-        "Termites can fart.",
-        "Mother chickens talk to their eggs",
-        "Animal Fact 3",
-        "Animal Fact 4",
-        "Animal Fact 5",
-        "Animal Fact 6",
-        "Animal Fact 7",
-        "Animal Fact 8",
-        "Animal Fact 9",
-        "Animal Fact 10"
+        "TERMITES CAN FART",
+        "MOTHER CHICKENS TALK TO THEIR EGGS.",
+        "THE PEACOCK MANTIS SHRIMP CAN THROW A PUNCH AT 50 MPH, ACCELERATING QUICKER THAN A .22-CALIBER BULLET.",
+        "ONLY THE MALES ARE CALLED PEACOCKS. FEMALES ARE CALLED PEAHENS.",
+        "ALBERTA, CANADA IS THE LARGEST RAT-FREE POPULATED AREA IN THE WORLD.",
+        "ALL CLOWNFISH ARE BORN MALE; SOME TURN FEMALE TO ENABLE MATING.",
+        "IN AN AQUATIC TRAFFIC JAM, ALLIGATORS WILL GIVE MANATEES THE RIGHT OF WAY.",
+        "CARIBBEAN SPERM WHALES HAVE THEIR OWN REGIONAL ACCENT.",
+        "THE CLOSEST LIVING RELATIVE TO THE TYRANNOSAURUS REX IS THE CHICKEN.",
+        "STARLINGS CAN IMITATE WORDS.",
+        "A SNAIL CAN SLEEP FOR THREE YEARS STRAIGHT.",
+        "BATS ALWAYS TURN LEFT WHEN LEAVING A CAVE."
     ];
     
     const randomFact = facts[Math.floor(Math.random()*facts.length)];
